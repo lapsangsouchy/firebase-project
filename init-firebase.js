46b3afddaa909508c52237373c5df17794391064
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2025-07-30 12:30:42 -0400

    First Initial Finished Product

---
+++ init-firebase.js
@@ -0,0 +1,18 @@
+/**
+ * @TODO replace the firebaseConfig object below with the one
+ * provided for you by firebase after creating your project!
+ */
+const firebaseConfig = {
+  // This info will be different for your Firebase database
+  apiKey: 'AIzaSyCNgPDd-DA1fy6eltmdJCvxPbuPHXsr1lc',
+  authDomain: 'once-again-testing-fire-chat.firebaseapp.com',
+  databaseURL:
+    'https://once-again-testing-fire-chat-default-rtdb.firebaseio.com',
+  projectId: 'once-again-testing-fire-chat',
+  storageBucket: 'once-again-testing-fire-chat.firebasestorage.app',
+  messagingSenderId: '818393207043',
+  appId: '1:818393207043:web:a8bde6b7ccb7c07e8bd936',
+};
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
---
+++ script.js
@@ -0,0 +1,278 @@
+/**
+ * Reference to the Firebase Database object
+ */
+
+const database = firebase.database().ref();
+
+/**
+ * References to the following elements:
+ *      - div with id #all-messages
+ *      - input with id #username
+ *      - input with id #message
+ */
+
+const allMessages = document.getElementById('all-messages');
+const usernameElem = document.getElementById('username');
+const messageElem = document.getElementById('message');
+
+/**
+ *  Add Reference to Email Input
+ */
+
+const emailElem = document.getElementById('email');
+
+/**
+ *  Add Reference to Profile Image Input
+ */
+
+const profileElem = document.getElementById('profile');
+
+/**
+ * Reference to the sendBtn with onclick event listener updateDB
+ */
+
+const sendBtn = document.getElementById('send-btn');
+sendBtn.onclick = updateDB;
+
+async function updateDB(event) {
+  // Prevent default refresh
+  event.preventDefault();
+
+  /* Add a check to see if any important fields are empty and if so, send an alert */
+  if (
+    usernameElem.value == '' ||
+    messageElem.value == '' ||
+    emailElem.value == ''
+  ) {
+    alert('Please enter something');
+  } else {
+    /* Time handling! */
+
+    // Get the UNIX timestamp
+    let now = new Date();
+
+    console.log(now);
+
+    let date = now.toLocaleDateString();
+
+    let time = now.toLocaleTimeString();
+
+    // To only include hours and minutes, add:
+    //
+    //  navigator.language, {
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    // }
+    //
+    // to parameter of toLocaleTimeString();
+
+    /* Profile Image Handling */
+
+    // Declare a profile variable we can either use user input for, or a random Dog
+    let profile;
+
+    // If profileElem.value has value, store in variable for database
+
+    if (profileElem.value) {
+      profile = profileElem.value;
+    } else {
+      // Otherwise, select a random image from Dog API
+      await fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function (response) {
+          // .json() parses a JSON file so it's usable by JavaScript
+          return response.json();
+        })
+        .then(function (myJSON) {
+          // Assign profile to random dog url (inside "message")
+          profile = myJSON.message;
+        });
+    }
+
+    // Create data object
+    const data = {
+      USERNAME: usernameElem.value,
+      EMAIL: emailElem.value,
+      MESSAGE: messageElem.value,
+      DATE: date,
+      TIME: time,
+      PROFILE: profile,
+    };
+
+    // console.log the object
+    console.log(data);
+
+    // GET *PUSH* PUT DELETE
+    // Write to our database
+    database.push(data);
+
+    // Reset message AND keep profile image the same
+    messageElem.value = '';
+    profileElem.value = profile;
+  }
+}
+
+/**
+ * Add the addMessageToBoard function as an event
+ * handler for the "child_added" event on the database
+ * object
+ */
+
+database.on('child_added', addMessageToBoard);
+
+function addMessageToBoard(rowData) {
+  // Store the values of rowData inside object named 'data'
+  const data = rowData.val();
+
+  // console.log data
+  console.log(data);
+
+  // Create a variable named singleMessage
+  // that stores function call for makeSingleMessageHTML()
+  let singleMessage = makeSingleMessageHTML(
+    data.USERNAME,
+    data.EMAIL,
+    data.MESSAGE,
+    data.DATE,
+    data.TIME,
+    data.PROFILE
+  );
+
+  // Append the new message HTML element to allMessages
+  allMessages.append(singleMessage);
+}
+
+function makeSingleMessageHTML(
+  usernameTxt,
+  emailTxt,
+  messageTxt,
+  dateTxt,
+  timeTxt,
+  profileTxt
+) {
+  // Create Parent Div
+  let parentDiv = document.createElement('div');
+  // Add Class name .single-message
+  parentDiv.className = 'single-message';
+
+  /* NEW HEAD DIV FOR USERNAME, EMAIL, & TIMESTAMP */
+
+  // Create Head Div
+  let headDiv = document.createElement('div');
+  // Add Class name single-message-head
+  headDiv.className = 'single-message-head';
+
+  /* DATE AND TIME ELEMENTS */
+
+  // Create Date P Tag
+  let dateP = document.createElement('p');
+  // Add the date data to P tag
+  dateP.innerHTML = dateTxt;
+  // Add class name single-message-time to dateP
+  dateP.className = 'single-message-time';
+
+  // Create Time P Tag
+  let timeP = document.createElement('p');
+  // Add the time data to P tag
+  timeP.innerHTML = timeTxt;
+  // Add class name single-message-time to timeP
+  timeP.className = 'single-message-time';
+
+  // Create Username P Tag
+  let usernameP = document.createElement('p');
+  usernameP.className = 'single-message-username';
+  usernameP.innerHTML = usernameTxt + ':';
+
+  /* EMAIL ELEMENT */
+  // Create Email P Tag
+  let emailP = document.createElement('p');
+  emailP.className = 'single-message-email';
+  emailP.innerHTML = emailTxt;
+
+  headDiv.append(usernameP, emailP, dateP, timeP);
+
+  /* NEW IMAGE ELEMENT */
+
+  // Create Profile Img element
+  let profileImg = document.createElement('img');
+  // Add Class name single-message-img
+  profileImg.className = 'single-message-img';
+  // Add database URL to src
+  profileImg.src = profileTxt;
+
+  // Create message P Tag
+  let messageP = document.createElement('p');
+  messageP.innerHTML = messageTxt;
+  parentDiv.append(profileImg, headDiv, messageP);
+
+  // Return Parent Div
+  return parentDiv;
+}
+
+/**
+ * @BONUS add an onkeyup event handler to the form HTML
+ * element so the user can also submit the form with the
+ * Enter key
+ *
+ * @BONUS use an arrow function
+ *
+ */
+
+const handleEnterKey = (event) => {
+  if (event.key === 'Enter') {
+    updateDB(event);
+  }
+};
+
+usernameElem.addEventListener('keydown', handleEnterKey);
+messageElem.addEventListener('keydown', handleEnterKey);
+
+/**
+ * FOR INSTRUCTOR PURPOSES ONLY - CLEARING MESSAGES EVERY 30 MINUTES
+ */
+// Function to clear the chat messages (you don't need to teach this!)
+function clearChat() {
+  // Clear the Firebase database
+  database
+    .remove()
+    .then(() => {
+      // Clear the chat display
+      allMessages.innerHTML = '';
+      console.log('Chat messages cleared successfully.');
+    })
+    .catch((error) => {
+      console.error('Error clearing chat messages:', error);
+    });
+}
+
+// Set a timeout to clear chat messages after fifteen minutes (900000 milliseconds)
+setTimeout(clearChat, 900000);
+
+/* For Checking Images */
+
+/**
+ * You can actually check to see if an image that was uploaded is a valid image or not
+ *
+ * To do so, there's a nifty function someone wrote up as a solution
+ * on Stackoverflow:
+ *
+ * You can add this function in your code:
+ * 
+ * function checkImage(imageSrc, good, bad) {
+    var img = new Image();
+    img.onload = good;
+    img.onerror = bad;
+    img.src = imageSrc;
+  }
+ *  
+ * And it'll check if your image loads or causes an error
+ * If the 'good' and 'bad' parameters can be functions that you use to return something
+ * So if the image loads (onload) you can call a function that returns TRUE
+ * If the image doesn't load (onerror) you can all a function that returns FALSE
+ * 
+ * That way, you can check in a conditional to see if the image is valid like so:
+ * 
+ * if (checkImage(profileElem.value, function () {return true;}, function () {return false;}))
+ * 
+ * 
+ * 
+*/
